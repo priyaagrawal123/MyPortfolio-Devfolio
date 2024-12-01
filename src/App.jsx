fab34a81b3228a95eda9ce9816b985ae31e25fc0
priyaagrawal123 <147872840+priyaagrawal123@users.noreply.github.com>
2024-12-01 15:21:16 +0530

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -11,43 +11,19 @@ import Contact from './components/Contact';
 function App() {
   return (
     <div className="w-full h-screen  bg-[url('./assets/girl2.avif')] opacity-80 bg-cover bg-blend-overlay">
+      <Navbar />
       <Routes>
-        <Route path="/" element={<div><Navbar /><Title /></div>} />
-        <Route path="/title" element={<div><Navbar /><Title /></div>} />
-        <Route path="/project" element={<div><Navbar /><Project /></div>} />
-        {/* <Route path="/navbar " element={<Navbar />} />
-        <Route path="/social " element={<Social />} />
-        <Route path="/title " element={<Title />} />
-        <Route path="/project" element={<Project />} /> */}
-        <Route path="/about" element={<div><Navbar /><About /></div>} />
-        <Route path="/resume" element={<div><Navbar /><Resume /></div>} />
-        <Route path="/contact" element={<div><Navbar /><Contact /></div>} />
+        <Route path="/" element={<div><Title /></div>} />
+        <Route path="/title" element={<div><Title /></div>} />
+        <Route path="/project" element={<div><Project /></div>} />
+        <Route path="/about" element={<div><About /></div>} />
+        <Route path="/resume" element={<div><Resume /></div>} />
+        <Route path="/contact" element={<div><Contact /></div>} />
       </Routes>
     </div>
   );
 }
 
 export default App;
-// import './App.css';
-// import Navbar from "./components/Navbar";
-// import Title from "./components/Title";
-// import Social from "./components/Social";
-// import Project from "./components/Project";
-// import { Routes, Route } from 'react-router-dom';
-// import About from './components/About';
 
-// function App() {
-//   return (
-//     <div className="relative w-full h-screen bg-cover bg-center" style={{ backgroundImage: "url('./assets/girl2.avif')" }}>
-//       <div className="absolute inset-0 bg-blue-300 bg-opacity-[-20]"></div> {/* Light blue overlay with even lower opacity */}
-//       <Routes>
-//         <Route path="/" element={<div><Navbar /><Social /><Title /></div>} />
-//         <Route path="/project" element={<Project />} />
-//         <Route path="/about" element={<About />} />
-//       </Routes>
-//     </div>
-//   );
-// }
-
-// export default App;
 
